f5a7bcc8db8b16fccc0fe0bc7d1ef6097d2e4d69
SaschaSchuster <saschust@gmail.com>
2014-10-24 23:48:00 +0200

    renamed the .csv file to read
    Because of time pressure, we conduct redundant operations, but that does
    no damage. Redundancy is good. Redundancy assures you. That is why
    people go to church every week.

--- Plots.R
+++ Plots.R
@@ -14,7 +14,7 @@
 ################################
 
 #Load basic R packages
-library(foreign) 
+library(foreign)
 library(car)
 library(ggplot2)
 
@@ -22,17 +22,17 @@ library(ggplot2)
 setwd("C:/Users/Lokus/Dropbox/Master Thesis/GitHub-Repo/UrbanTerror")
 
 #Load the Global Terrorism Database
-rawGTD <- read.csv("globalterrorismdb_0814dist.csv", header=TRUE)
+rawGTD <- read.csv("GTD.csv", header=TRUE)
 
-#The Global Terror Database (GTD) we are using for our analysis contains over a 120k observations on more than 120 variables. 
+#The Global Terror Database (GTD) we are using for our analysis contains over a 120k observations on more than 120 variables.
 #The complete database contains redundant or dispensable data for our analysis.
-#We therefore filter the database to make it fit our needs. We erase over a 100 variables. 
-#We only want to look at successfull terror attacks and include basic data on time, location and target.  
+#We therefore filter the database to make it fit our needs. We erase over a 100 variables.
+#We only want to look at successfull terror attacks and include basic data on time, location and target.
 
 GTD <- subset(rawGTD, select = c(eventid, iyear, imonth, iday, country, region, attacktype1, targtype1, targsubtype1,
                                   propextent, nkill, nwound),iyear >= 1970 & success == 1, na.strings = c("", " "))
 
-### We introduce our first scale: "Targets Urbanity Potential Scale (TUPscale)" 
+### We introduce our first scale: "Targets Urbanity Potential Scale (TUPscale)"
 GTD["TUPscale"] <- GTD$targsubtype1
 GTD$TUPscale <- recode(GTD$TUPscale, "40:42 = 3; 9 = 1; 27:35 = 1; 37:39 = 1; 65 = 1; 72 = 1; 1 = 2; 4:5 = 2; 10 = 2;
                        12 = 2; 53:56 = 2; 58:59 = 2; 61:62 = 2; 82 = 2; 95:96 = 2;6 = 3; 13 = 3;
@@ -50,21 +50,21 @@ GTD$PROPscale <- as.numeric(GTD$PROPscale)
 GTD["HUMscale"] <- GTD$nkill+GTD$nwound
 GTD$HUMscale <- as.numeric(GTD$HUMscale)
 
-#Bring down to self explanatory values for the Plot 
+#Bring down to self explanatory values for the Plot
 PROPforPlot <- recode(GTD$PROPscale, "1= '> Billion $'; 2= '> Million $'; 3='< Million $'; 4='no economic damage'; NA=NA; 0=NA")
 
 # In oder to use it in the later charts we recode from the arbitrary 1:5 to a new arbitrary scale that indicates
-# a level of urbanity alreadey. The new element is introduced to become part of a dataframe for plotting. 
+# a level of urbanity alreadey. The new element is introduced to become part of a dataframe for plotting.
 
-TUPforPlot <- recode(GTD$TUPscale, "1 = '0 - Military and Farmland'; 2 = '2 - Local Governanceand Police'; 
+TUPforPlot <- recode(GTD$TUPscale, "1 = '0 - Military and Farmland'; 2 = '2 - Local Governanceand Police';
                      3 = '5 - Potentially Urban Workplace'; 4 = '7 - Potentially Urban Infrastructure'; 5
                      = '9 - Potentially Expressions of Urban Life'", as.numeric.result=FALSE)
 
 
 #prepare a data frame for plotting
 Plotframe <- data.frame(year=GTD$iyear, TUP=TUPforPlot, PROP=PROPforPlot, HUM=GTD$HUMscale)
-  
-#as we want to get a sense if different collection issues merged in the GTD, we will split plots with 
+
+#as we want to get a sense if different collection issues merged in the GTD, we will split plots with
 # horizontal lines that indicate an transition on collecting entity (PIGS, CETIS, ISVG, START)
 intersepz=data.frame(date=as.numeric(c("27", "38", "41")), date2=as.numeric(c("1997", "2009", "2011")), event=c("PGIS Data Collection End", "CETIS Data Collection End", "ISVG Data Collection End"))
 
@@ -72,7 +72,7 @@ intersepz=data.frame(date=as.numeric(c("27", "38", "41")), date2=as.numeric(c("1
 qplot(factor(year), data=Plotframe, geom = "freqpoly", color = TUP, group = TUP, ylab= "sum of attacks", xlab= "year",
 main="Sum of attacks per year
 with indication for the transition on collecting entities:
-PIGS until 1997, CETIS until 2008, ISVG until 2011 and START since 2011") + geom_vline(data=intersepz, 
+PIGS until 1997, CETIS until 2008, ISVG until 2011 and START since 2011") + geom_vline(data=intersepz,
                                                                                        mapping=aes(xintercept=date))
 
 # the second plot shows the count of attack in different categories of economic damage
@@ -82,16 +82,8 @@ with indication for the transition on collecting entities:
 PIGS until 1997, CETIS until 2008, ISVG until 2011 and START since 2011") + geom_vline(data=intersepz,
                                                                                        mapping=aes(xintercept=date))
 # the third plot shows the sum of killed and injured of attacks per year
-qplot(year, HUM, data=Plotframe, stat="summary", fun.y="sum", geom ="line", 
-ylab= "sum of deaths and injured", main="Sum of casualties per year 
+qplot(year, HUM, data=Plotframe, stat="summary", fun.y="sum", geom ="line",
+ylab= "sum of deaths and injured", main="Sum of casualties per year
 with indication for the transition on collecting entities:
-PIGS until 1997, CETIS until 2008, ISVG until 2011 and START since 2011") + geom_vline(data=intersepz, 
+PIGS until 1997, CETIS until 2008, ISVG until 2011 and START since 2011") + geom_vline(data=intersepz,
                                                                                        mapping=aes(xintercept=date2))
-
-
-
-
-
-
-
-
